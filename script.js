a068b85108cda1e17a8cce11e2c0ba97d397df8c
w3punk <w3punk@pm.me>
2019-05-31 18:22:38 +0300

    Switch to react

--- script.js
+++
@@ -1,5 +0,0 @@
-const expand = event => event.currentTarget.classList.add('expand');
-
-document
-  .querySelectorAll('.cell')
-  .forEach(cell => cell.addEventListener('click', event => expand(event)));
